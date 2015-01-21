10ef89eb83c3eaf283612210197ee0a1a3dd836b
jhonnymoreira <xDeniedAccess@gmail.com>
2015-01-21 04:34:43 -0200

    Added 'main.js' with all the functions and comma first applied to 'hex-to-rgb.js'

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-function rgbColor(hex) {
+var playground = { version: 1.0 };
+
+playground.fibonacci = function () {
+  var begin = 1
+    , middle = 0
+    , end = 0
+    , fibonacci = []
+    , sequence = prompt('How many numbers in the sequence?');
+
+  for ( i = 0; i < sequence; i++ ) {
+    end = begin + middle;
+    fibonacci[i] = end;
+
+    console.log( fibonacci[i] );
+
+    begin = middle;
+    middle = end;
+  }
+};
+
+playground.hexToRGB = function (hex) {
   var base = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
       , hex  = hex.toLowerCase().split('')
       , rgb  = [];
@@ -14,4 +34,4 @@ function rgbColor(hex) {
   }
 
   return rgb;
-}
+};
