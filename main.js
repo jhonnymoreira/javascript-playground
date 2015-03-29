00c0c9f13917811ba42730b3e3e133dccd22b8ee
Jhonny Moreira <xDeniedAccess@gmail.com>
2015-03-29 08:36:56 -0300

    Added evenFibonacci function to main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var playground = { version: 1.0 };
 
 playground.fibonacci = function () {
-  var begin = 1
-    , middle = 0
-    , end = 0
-    , fibonacci = []
-    , sequence = prompt('How many numbers in the sequence?');
+  var begin = 1;
+  var middle = 0;
+  var end = 0;
+  var fibonacci = [];
+  var sequence = prompt('How many numbers in the sequence?');
 
   for ( i = 0; i < sequence; i++ ) {
     end = begin + middle;
@@ -19,9 +19,9 @@ playground.fibonacci = function () {
 };
 
 playground.hexToRGB = function (hex) {
-  var base = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-      , hex  = hex.toLowerCase().split('')
-      , rgb  = [];
+  var base = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+  var hex  = hex.toLowerCase().split('');
+  var rgb  = [];
 
   if ( hex.length <= 3 ) {
     for ( i = 0; i <= 2; i++ ) {
@@ -34,4 +34,26 @@ playground.hexToRGB = function (hex) {
   }
 
   return rgb;
+};
+
+playground.evenFibonnaci = function () {
+  var a = 1;
+  var b = 0;
+  var c = 0;
+  var s = 0;
+  var i = 0;
+
+  while ( i < i + 1 ) {
+    c = a + b;
+
+    if ( c % 2 == 0 ) { s += c }
+    if ( c >= 4000000 ) { break }
+
+    a = b;
+    b = c;
+
+    i++;
+  }
+
+  return s;
 };
