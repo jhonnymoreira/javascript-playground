888da69ba48deee0b65563fa2bd134361ae4842d
Jhonny Moreira <xDeniedAccess@gmail.com>
2015-08-28 08:52:17 -0300

    Standardizing the code

--- even-fibonacci/even-fibonacci.js
+++ even-fibonacci/even-fibonacci.js
@@ -1,35 +1,22 @@
 function evenFibonacci() {
-  // Term n1
-  var a = 1;
-  // Term n2
-  var b = 0;
-  // Term n1 + n2
-  var c = 0;
-  // Setting up the limit in a variable
-  var l = 4000000;
-  // Sum of all even numbers in Fibonnaci sequence between 0 and 4.000.000
-  var s = 0;
-  // Loop usage
-  var i = 0;
+  var a = 1
+    , b = 0
+    , c = 0
+    , l = 4000000
+    , s = 0
+    , i = 0;
 
-  // Create an infinite loop until you get an number closer to 4.000.000
-  // Bigger than 4.000.000, loop stops.
   while (i < i + 1) {
     c = a + b;
 
-    // If even, add the value to sum variable
-    if (c % 2 == 0) { s += c }
-    // If bigger than limit (l), loop stops.
-    if (c >= l) { break }
+    if ( c%2 == 0 ) { s += c; }
+    if ( c >= l ) { break; }
 
-    // Fibonacci pattern
     a = b;
     b = c;
 
-    // Loop continues until the second condition (c >= 4x10^6) be true.
     i++;
   }
 
-  // The answer
   return s;
 }
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var playground = { version: 1.0 };
 
 playground.fibonacci = function () {
-  var begin = 1;
-  var middle = 0;
-  var end = 0;
-  var fibonacci = [];
-  var sequence = prompt('How many numbers in the sequence?');
+  var begin = 1
+    , middle = 0
+    , end = 0
+    , fibonacci = []
+    , sequence = prompt('How many numbers in the sequence?');
 
   for ( i = 0; i < sequence; i++ ) {
     end = begin + middle;
@@ -19,17 +19,18 @@ playground.fibonacci = function () {
 };
 
 playground.hexToRGB = function (hex) {
-  var base = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-  var hex  = hex.toLowerCase().split('');
-  var rgb  = [];
+  var base = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c',
+              'd', 'e', 'f']
+    , hex  = hex.toLowerCase().split('')
+    , rgb  = [];
 
   if ( hex.length <= 3 ) {
     for ( i = 0; i <= 2; i++ ) {
-      rgb[i] = base.indexOf(hex[i]) * 16 + base.indexOf(hex[i]);
+      rgb[i] = base.indexOf( hex[i] )*16 + base.indexOf( hex[i] );
     }
   } else {
     for ( i = 0; i <= hex.length / 3; i++ ) {
-      rgb[i] = base.indexOf(hex[i * 2]) * 16 + base.indexOf(hex[i * 2 + 1]);
+      rgb[i] = base.indexOf( hex[i*2] )*16 + base.indexOf( hex[i*2 + 1] );
     }
   }
 
@@ -37,18 +38,18 @@ playground.hexToRGB = function (hex) {
 };
 
 playground.evenFibonnaci = function () {
-  var a = 1;
-  var b = 0;
-  var c = 0;
-  var l = 4000000;
-  var s = 0;
-  var i = 0;
+  var a = 1
+    , b = 0
+    , c = 0
+    , l = 4000000
+    , s = 0
+    , i = 0;
 
   while (i < i + 1) {
     c = a + b;
 
-    if (c % 2 == 0) { s += c }
-    if (c >= l) { break }
+    if ( c%2 == 0 ) { s += c; }
+    if ( c >= l ) { break; }
 
     a = b;
     b = c;
@@ -57,4 +58,4 @@ playground.evenFibonnaci = function () {
   }
 
   return s;
-}
+};
